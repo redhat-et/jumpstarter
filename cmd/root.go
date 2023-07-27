28c192fc4f41b8f08bab61b5712833d3ade3b0c8
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2023-07-27 16:04:25 +0200

    Cosmetic cleanups of basic comands

--- cmd/root.go
+++ cmd/root.go
@@ -12,16 +12,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "jumpstarter",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "Jumpstarter is a tool to perform HIL testing on Edge and Embedded devices",
+	Long: `This commandline tool is the interface to the jumpstarter drivers
+which enable the control of Edge and Embedded devices from a host.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -34,13 +27,4 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jumpstarter.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
